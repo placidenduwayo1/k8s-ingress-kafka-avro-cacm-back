77474f79bfe39f5e886a1d5ca50817429ebfaa7c
pn-zbook-g3 <placide.nd@gmail.com>
2024-01-09 16:02:52 +0100

    feat( AddressInputServiceImplTest.java.java.java ): writing unit tests

---
+++ cacmer-bs-ms-modules/k8s-ingress-kafka-avro-clean-archi-cacmer-bs-ms-address/src/test/java/fr/placide/cacmbsmsaddress/infrastructure/inputport/web/AddressControllerTest.java
@@ -0,0 +1,37 @@
+package fr.placide.cacmbsmsaddress.infrastructure.inputport.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void gets() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void testGets() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
